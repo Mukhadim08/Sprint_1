9f4fd6f45471d4784bc08a833c670003468b8820
Mukhadim <mukhadim11@gmail.com>
2026-03-03 16:01:05 +0500

    Fix task_4

--- task_4.py
+++ task_4.py
@@ -4,6 +4,5 @@ completed_tasks = ['task_002', 'task_012', 'task_006']
 completed_tasks.append(new_tasks.pop())
 new_tasks.remove('task_007')
 
-print(new_tasks)
-print(completed_tasks)
+
 print(new_tasks[-1])
